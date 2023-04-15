609cf8373af4f800797e9bd6d2cde47efb674ee5
Shamim <shamim73124@gmail.com>
2023-04-15 13:26:40 +0530

    Changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,16 +8,15 @@ public class Main {
 
         System.out.println(a + " " + b + " " + c);
     }
-}
-
-class Product {
-    public int product(int x, int y) {
-        return x * y;
-    }
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-    public double product(double x, double y) {
-        return x * y;
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
     }
 }
